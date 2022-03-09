9de19f7683b00190e74523a91c2472874b8da12a
Rodrigo Vieira <rodrigovf21021994@gmail.com>
2022-03-09 13:43:18 -0300

    Simple Create Form

--- SallesWebMvc/Controllers/SellersController.cs
+++ SallesWebMvc/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SallesWebMvc.Services;
+using SallesWebMvc.Models;
 
 namespace SallesWebMvc.Controllers
 {
@@ -22,6 +23,18 @@ namespace SallesWebMvc.Controllers
             return View(list);
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
 
     }
 }
