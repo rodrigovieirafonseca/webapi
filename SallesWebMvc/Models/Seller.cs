2fab806f154f931fd377144a5f40f59aea9c3d9f
Rodrigo Vieira <rodrigovf21021994@gmail.com>
2022-03-09 20:21:10 -0300

    App locale, number and data formating

--- SallesWebMvc/Models/Seller.cs
+++ SallesWebMvc/Models/Seller.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace SallesWebMvc.Models
 {
@@ -11,8 +12,14 @@ namespace SallesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Display(Name="Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString ="{0:F2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; } 
