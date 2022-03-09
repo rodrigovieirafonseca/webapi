ea93b556bf3c958a0835dbfb66a80c7528ea3e8f
Rodrigo Vieira <rodrigovf21021994@gmail.com>
2022-03-09 20:48:16 -0300

    Validation

--- SallesWebMvc/Controllers/SellersController.cs
+++ SallesWebMvc/Controllers/SellersController.cs
@@ -40,6 +40,12 @@ namespace SallesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentServicce.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -100,7 +106,13 @@ namespace SallesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
-            if(id !=seller.Id)
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentServicce.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+            if (id !=seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
             }
